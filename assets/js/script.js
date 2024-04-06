0dc630a90f87dae09c47315e957380246d43c47a
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-06 19:15:30 +0200

    1.1.1 playing with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-document.getElementById('h1_header').addEventListener('click', () => {
-    alert('everything works!');
+const h1 = document.getElementById('h1_header');
+const eventPre = document.getElementById('event_pre');
+
+document.getElementById('h1_header').addEventListener('click', (event) => {
+    h1.innerHTML = 'everything works!';
+    //alert('everything works!');
+    eventPre.innerHTML = event.target;
+    console.log(event);
 });
+
+window.addEventListener('resize', (e) => {
+    e.preventDefault;
+    h1.innerHTML = window.innerWidth;
+})
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $headName = 'index';
 require_once __DIR__ . '/components/header.php';
 ?>
 <!-- end header -->
-
+<pre class="text-center" id="event_pre"></pre>
 </body>
 
 </html>
