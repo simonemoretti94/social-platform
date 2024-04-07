d9632db5b90c32a0abaab2f0e36b89ea0be292f3
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-07 13:07:14 +0200

    2.1th playing with js

--- assets/graph/ER-graph.php
+++ assets/graph/ER-graph.php
@@ -25,19 +25,32 @@ require_once __DIR__ . '/../../components/header.php';
             filter: drop-shadow(1px 1.5px 5px #9614dc98);
         }
     }
+
+    #graph-2,
+    #graph-3 img {
+        max-width: 100%;
+    }
+
+    #hr-divider {
+        margin: .5rem auto;
+        border: solid 2px black;
+    }
 </style>
 
 <!-- end header -->
 <main>
     <sup id="screen_details" style="margin: '.5rem auto .2rem auto;"></sup>
     <img id="graph-1" src="../img/ER-graph.png" alt="er-graph-drawio">
-    <div id="graph-2" style="display: none;">
+    <div id="graph-2" class="container-fluid m-auto bg-light   flex-column">
         <img src="./responsive-img/1on2_left.png" alt="1on2_left">
+        <hr id="hr-divider">
         <img src="./responsive-img/1on2_right.png" alt="1on2_right">
     </div>
-    <div id="graph-3" style="display: none;">
+    <div id="graph-3" class="container-fluid m-auto bg-light flex-column">
         <img src="./responsive-img/1on3_left.png" alt="1on3_left">
+        <hr id="hr-divider">
         <img src="./responsive-img/1on3_center.png" alt="1on3_center">
+        <hr id="hr-divider">
         <img src="./responsive-img/1on3_right.png" alt="1on3_right">
     </div>
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
 const supInfo = document.getElementById('screen_details');
-
-window.addEventListener('resize', (e) => {
-    e.preventDefault;
-    supInfo.innerHTML = 'w: ' + window.innerWidth + '&nbsp;h: ' + window.innerHeight;
-});
+const graph1 = document.getElementById('graph-1');
+const graph2 = document.getElementById('graph-2');
+const graph3 = document.getElementById('graph-3');
 
 
 document.getElementById('graph_redirect').addEventListener('click', (e) => {
     console.log('clicked: ', e.target);
+    graph2.style.display = 'none';
+    graph3.style.display = 'none';
+});
+
+//html sup viewport infos
+window.addEventListener('resize', (e) => {
+    e.preventDefault;
+    supInfo.innerHTML = 'w: ' + window.innerWidth + '&nbsp;h: ' + window.innerHeight;
+
+    //er-graph page img renderer
+    if (window.innerWidth > 800) {
+        graph1.style.display = 'block';
+        graph2.style.display = 'none';
+        graph3.style.display = 'none';
+    }
+    else if (window.innerWidth < 800) {
+        graph1.style.display = 'none';
+        graph2.style.display = 'block';
+        graph3.style.display = 'none';
+    }
+    else if (window.innerWidth < 450) {
+        graph1.style.display = 'none';
+        graph2.style.display = 'none';
+        graph3.style.display = 'block';
+    }
+    else {
+        graph1.style.display = 'none';
+    }
 });
