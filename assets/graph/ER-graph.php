d141f918579b086fbbfba18ca65a003100055292
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-07 17:14:14 +0200

    2.2th er-graph rendering completed

--- assets/graph/ER-graph.php
+++ assets/graph/ER-graph.php
@@ -16,6 +16,10 @@ require_once __DIR__ . '/../../components/header.php';
         place-content: center;
         overflow-y: scroll;
 
+        >sup {
+            cursor: pointer;
+        }
+
         & img#graph-1 {
             width: calc(100vw - 40px);
             height: calc(100vh - 120px);
@@ -26,9 +30,19 @@ require_once __DIR__ . '/../../components/header.php';
         }
     }
 
-    #graph-2,
+    #graph-2 img,
     #graph-3 img {
         max-width: 100%;
+    }
+
+    .flex-column {
+        display: flex;
+        flex-direction: column;
+
+        & img {
+            flex: 100%;
+            padding: 1rem;
+        }
     }
 
     #hr-divider {
@@ -41,12 +55,14 @@ require_once __DIR__ . '/../../components/header.php';
 <main>
     <sup id="screen_details" style="margin: '.5rem auto .2rem auto;"></sup>
     <img id="graph-1" src="../img/ER-graph.png" alt="er-graph-drawio">
-    <div id="graph-2" class="container-fluid m-auto bg-light   flex-column">
+
+    <div id="graph-2" class="container-md m-auto bg-light   flex-column">
         <img src="./responsive-img/1on2_left.png" alt="1on2_left">
         <hr id="hr-divider">
         <img src="./responsive-img/1on2_right.png" alt="1on2_right">
     </div>
-    <div id="graph-3" class="container-fluid m-auto bg-light flex-column">
+
+    <div id="graph-3" class="container-md m-auto bg-light flex-column">
         <img src="./responsive-img/1on3_left.png" alt="1on3_left">
         <hr id="hr-divider">
         <img src="./responsive-img/1on3_center.png" alt="1on3_center">
@@ -55,6 +71,15 @@ require_once __DIR__ . '/../../components/header.php';
     </div>
 
 </main>
+
+<script>
+    const graph_2 = document.getElementById('graph-2');
+    const graph_3 = document.getElementById('graph-3');
+    document.addEventListener('DOMContentLoaded', (e) => {
+        graph_2.style.display = 'none';
+        graph_3.style.display = 'none';
+    });
+</script>
 
 
 <!-- importing footer  contains body's close tag -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,25 @@ const graph1 = document.getElementById('graph-1');
 const graph2 = document.getElementById('graph-2');
 const graph3 = document.getElementById('graph-3');
 
-
-document.getElementById('graph_redirect').addEventListener('click', (e) => {
-    console.log('clicked: ', e.target);
-    graph2.style.display = 'none';
-    graph3.style.display = 'none';
-});
-
 //html sup viewport infos
 window.addEventListener('resize', (e) => {
-    e.preventDefault;
+    //e.preventDefault;
     supInfo.innerHTML = 'w: ' + window.innerWidth + '&nbsp;h: ' + window.innerHeight;
 
-    //er-graph page img renderer
-    if (window.innerWidth > 800) {
-        graph1.style.display = 'block';
-        graph2.style.display = 'none';
-        graph3.style.display = 'none';
-    }
-    else if (window.innerWidth < 800) {
-        graph1.style.display = 'none';
-        graph2.style.display = 'block';
-        graph3.style.display = 'none';
-    }
-    else if (window.innerWidth < 450) {
+    if (window.innerWidth < 550) {
         graph1.style.display = 'none';
         graph2.style.display = 'none';
         graph3.style.display = 'block';
-    }
-    else {
-        graph1.style.display = 'none';
+        console.log('g1: ', graph1, 'g2: ', graph2, 'g3: ', graph3);
+    } else if (window.innerWidth < 800) {
+        //graph1.style.display = 'none';
+        graph2.style.display = 'block';
+        graph3.style.display = 'none';
+        console.log('g1: ', graph1, 'g2: ', graph2, 'g3: ', graph3);
+    } else if (window.innerWidth > 800) {
+        graph1.style.display = 'block';
+        graph2.style.display = 'none';
+        //graph3.style.display = 'none';
+        console.log('g1: ', graph1, 'g2: ', graph2, 'g3: ', graph3);
     }
 });
