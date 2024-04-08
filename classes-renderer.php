7bad09bf6e073790d55e7d2ce0afe4172d472924
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-08 22:26:26 +0200

    4.5th created first posts card

--- classes-renderer.php
+++ classes-renderer.php
@@ -18,6 +18,25 @@ $headName = 'classes-renderer';
 
     .container {
         overflow-y: scroll;
+
+        div#card {
+            #card-header {
+                max-height: 300px;
+
+                >img#card-img {
+                    max-width: 100%;
+                    max-height: 100%;
+                }
+            }
+
+            /* & #card-body{
+
+            }
+
+            & #card-footer {
+
+            } */
+        }
     }
 </style>
 
@@ -41,20 +60,26 @@ require_once __DIR__ . '/components/header.php';
         <?php
         var_dump($posts);
         ?>
-        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-12">
-            <div class="card p-1">
-                <div class="card-header">
-                    <img class="card-img-top" src="" alt="sea_img">
-                </div>
-                <div class="card-body">
-                    <p class="card-text"></p> <!-- title -->
-                    <p></p><!-- date -->
-                </div>
-                <div class="card-footer">
-                    <p><span></span></p><!-- tags -->
+
+        <?php foreach ($posts as $post) : ?>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                <div id="card" class="card p-1">
+                    <div id="card-header" class="card-header">
+                        <img id="card-img" class="card-img-top" src="https://wallpapercave.com/wp/wp3250456.jpg" alt="sea_img">
+                    </div>
+                    <div id="card-body" class="card-body">
+                        <p class="card-text"><?= $post->title ?></p> <!-- title -->
+                        <p><?= $post->date ?></p>
+                        </p><!-- date -->
+                    </div>
+                    <div id="card-footer" class="card-footer">
+                        <?php foreach ($post->tags as $tag) : ?>
+                            <p><?= '<span>' . $tag . '</span>'; ?></p><!-- tags -->
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
 
     <hr>
