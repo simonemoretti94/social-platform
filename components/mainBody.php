ecd6335cb9ee25efb374dabe1b00c476af4e1e0b
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-08 12:31:40 +0200

    3.6th styling users into DOM'UI

--- components/mainBody.php
+++ components/mainBody.php
@@ -3,12 +3,52 @@
         padding: 1rem;
 
         >div#user {
+            /* positioning items */
             display: flex;
             flex-direction: column;
-            flex-wrap: wrap;
+            justify-content: space-around;
+
+            /* styling box */
+            border: solid .5px white;
+            border-radius: 8px;
+            box-shadow: 0 0 3px white;
 
             & * {
                 text-wrap: wrap;
+            }
+
+            #username,
+            #user-id,
+            #user-likes {
+                text-align: center;
+
+                >strong {
+                    color: var(--blue);
+                }
+
+                & span {
+                    font-style: italic;
+                    font-size: medium;
+                }
+            }
+
+            div.wrapper {
+                display: flex;
+
+
+                & p#user-id,
+                p#user-likes {
+                    width: 50%;
+                }
+            }
+
+        }
+
+        >div#user:hover {
+            background-color: var(--blue-light);
+
+            & span {
+                color: var(--dark-light);
             }
         }
     }
@@ -20,10 +60,14 @@
 
     if ($result && $result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) : ?>
-            <!-- Destructuring (verify var_dupm($row))
-            ['id' => id, 'name' => name,] = $row; -->
             <div id="col" class="col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                <div id="user" class="container-fluid"><?php echo var_dump($row) ?></div>
+                <div id="user" class="container-fluid">
+                    <p id="username"><strong>user: </strong><span><?= $row['username']; ?></span></p>
+                    <div class="wrapper">
+                        <p id="user-id"><strong>id: </strong> <span><?= $row['id']; ?></span></p>
+                        <p id="user-likes"><strong>likes: </strong><span><?= $row['tot_likes'] ?><span></p>
+                    </div>
+                </div>
             </div>
     <?php endwhile;
     } else if ($result) {
