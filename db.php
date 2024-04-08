2015debbe66d464713d4f2db201c894e93e797f6
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-08 10:11:36 +0200

    3th ready for UI data printing

--- db.php
+++ db.php
@@ -1,28 +1,34 @@
 <?php
 
+// 1. Defyining constants
 define('DB_SERVERNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'social-platform');
 
+// 2. Defyining a new connection's instance
 $connection = null;
-
 $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//var_dump($connection);
 
-var_dump($connection);
-
+// 3. Checking if an error occurred
 if ($connection && $connection->connect_error) {
-    echo 'Dataabase Connection Faild' . $connection->connect_error;
+    echo 'Connection Failed | ' . $connection->connect_error;
     die;
-}
+};
 
+// 4. Querying the database
 $sql = null;
 $result = null;
 $row = null;
 
-$sql = 'phpMyAdmin sql command';
+// query: 5. Ordina gli utenti per totale di likes ricevuti nei loro posts (25);
+$sql = 'SELECT `users`.`username` , `users`.`id`, COUNT(`likes`.`post_id`) AS `tot_likes`
+FROM `users` JOIN `posts` ON `users`.`id` = `posts`.`user_id`
+JOIN `likes` ON `posts`.`id` = `likes`.`post_id`
+GROUP BY `users`.`id`
+ORDER BY `tot_likes` DESC;';
 $result = $connection->query($sql);
 
-while ($row = $result->fetch_assoc()) {
-    //['id' => id, 'name' => name, 'website' => website] = row;
-}
+
+$connection->close();
--- index.php
+++ index.php
@@ -6,12 +6,32 @@ $headName = 'index';
 <?php
 require_once __DIR__ . '/components/header.php';
 ?>
+
+<style scoped>
+    main {
+        min-height: calc(100vh - 100px);
+        max-height: calc(100vh - 100px);
+        overflow-y: scroll;
+    }
+</style>
 <!-- end header -->
 <main>
     <sup id="screen_details"></sup>
     <?php
     require_once __DIR__ . '/db.php';
-    echo '<div id="db-result">' . var_dump($result) . '</div>'
+    echo '<div id="db-result">' . var_dump($result) . '</div>' . '<hr><hr>';
+
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) : ?>
+            <!-- Destructuring (verify var_dupm($row))
+            ['id' => id, 'name' => name,] = $row; -->
+            <div id="user" class="container-fluid"><?php echo var_dump($row) ?></div>
+    <?php endwhile;
+    } else if ($result) {
+        echo '0 results';
+    } else {
+        echo 'query error';
+    };
     ?>
 </main>
 
